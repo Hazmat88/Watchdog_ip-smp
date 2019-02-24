9f161b75d7af5f6bd5b6c90ddf15248e13cc7658
Hazmat88_Desk <matthieu.vinet.hz@gmail.com>
2019-02-25 00:36:43 +0100

    First Initialisation Watchdog Ip/smp Project 1st

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta  charset="utf-8">
+     <!-- <meta http-equiv="refresh" content="1"> -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Watchdog_ip/smp</title>
+  </head>
+
+
+
+<body>
+    <?php
+        $date = strftime('%A %d %B %Y');
+        $heure = date("H:i:s");
+
+        echo "<h2>$date</h2>";
+    ?>
+
+        <h1 id="clock"></h1>
+
+
+
+      <div class="camera">
+
+            <div class="wiki">
+                <button class="button button1">Wiki___Switch</button>
+                <div class="cam01"><img id="browser_video" class="cam01" alt="video" src="http://192.168.1.37:8080/video"></div>
+            </div>
+
+            <div class="sony">
+                <button class="button button2">Sony___Switch</button>
+                <div class="cam02"><img id="browser_video" class="cam02" alt="video" src="http://192.168.1.37:8080/video"></div>
+            </div>
+
+      </div>
+
+
+</body>
+
+   <script src="js/main.js"></script>
+</html>
---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+//alert("hello_world");
+
+
+// raccourci d’écriture
+function $(id){return document.getElementById(id)}
+
+function hms(){
+var today=new Date();
+var hrs=today.getHours(),mns=today.getMinutes(),scd=today.getSeconds();
+var str=(hrs<10?"0"+hrs:hrs)+":"+(mns<10?"0"+mns:mns)+":"+(scd<10?"0"+scd:scd);
+$("clock").innerHTML=str;
+setTimeout(hms,1000);// réécriture toutes les 1000 millisecondes
+}
+hms();// lancement de la fonction
