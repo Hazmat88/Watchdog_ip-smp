c3f71cc02b5fafc757226b26089a4be6db3c6ba8
Hazmat88_Desk <matthieu.vinet.hz@gmail.com>
2019-02-25 03:23:35 +0100

    Adding audio wave format try to add selection video & audio format fail because they use a script and other extern tools, try to include but include import only the html/php page not the extern compenenent necesarry to interaction, but good news we can get the sources files like the scripts and other compenent wait & see

--- index.php
+++ index.php
@@ -25,18 +25,31 @@
 
             <div class="wiki">
                 <button class="button button1">Wiki___Switch</button>
-                <div class="cam01"><img id="browser_video" class="cam01" alt="video" src="http://192.168.1.37:8080/video"></div>
+                <div class="cam01"><img id="browser_video" class="cam01" alt="video" src="http://192.168.1.37:8080/video">
+                </div>
+                <div id="audio_pane" style="margin: auto; display: table;">
+                    <audio id="html5_audio" controls=""><source src="http://192.168.1.37:8080/audio.wav" type="audio/wav"></audio>
+                </div>
             </div>
 
             <div class="sony">
                 <button class="button button2">Sony___Switch</button>
-                <div class="cam02"><img id="browser_video" class="cam02" alt="video" src="http://192.168.1.37:8080/video"></div>
+                <div class="cam02"><img id="browser_video" class="cam02" alt="video" src="http://192.168.1.37:8080/video">
+                </div>
+                <div id="audio_pane" style="margin: auto; display: table;">
+                    <audio id="html5_audio" controls=""><source src="http://192.168.1.37:8080/audio.wav" type="audio/wav"></audio>
+                </div>
             </div>
 
       </div>
 
 
+
+
+
+
 </body>
 
    <script src="js/main.js"></script>
+   <script src="js/ipwebcam.js"></script>
 </html>
